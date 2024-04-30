b8a8142488bf89b31b21744b64dcf0f373088fb4
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-04-30 19:31:19 -0300

    Fitness App starting

---
+++ app/HomePage.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+import React from "react"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+export default function HomePage() {
+	return (
+		<SafeAreaView>
+			<Text style={{ color: "white" }}>HomePage</Text>
+		</SafeAreaView>
+	)
+}
