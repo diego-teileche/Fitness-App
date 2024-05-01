28010e50d92d1dd95a39def6f51a35f70b317bd7
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-05-01 20:03:56 -0300

    WelcomePage | Index added and welcome page finished without animations yet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,55 +1,6 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome"
-import {
-	DarkTheme,
-	DefaultTheme,
-	ThemeProvider,
-} from "@react-navigation/native"
-import { useFonts } from "expo-font"
+import React from "react"
 import { Stack } from "expo-router"
-import * as SplashScreen from "expo-splash-screen"
-import { useEffect } from "react"
 
-import { useColorScheme } from "@/components/useColorScheme"
-
-export { ErrorBoundary } from "expo-router"
-
-export const unstable_settings = {
-	initialRouteName: "(tabs)",
-}
-
-SplashScreen.preventAutoHideAsync()
-
-export default function RootLayout() {
-	const [loaded, error] = useFonts({
-		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-		...FontAwesome.font,
-	})
-
-	useEffect(() => {
-		if (error) throw error
-	}, [error])
-
-	useEffect(() => {
-		if (loaded) {
-			SplashScreen.hideAsync()
-		}
-	}, [loaded])
-
-	if (!loaded) {
-		return null
-	}
-
-	return <RootLayoutNav />
-}
-
-function RootLayoutNav() {
-	const colorScheme = useColorScheme()
-
-	return (
-		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-			<Stack>
-				<Stack.Screen name="HomePage" options={{ headerShown: false }} />
-			</Stack>
-		</ThemeProvider>
-	)
+export default function _layout() {
+	return <Stack screenOptions={{ headerShown: false }} />
 }
---
+++ app/index.tsx
@@ -0,0 +1,57 @@
+import { View, Text, Image, TouchableOpacity } from "react-native"
+import React from "react"
+import {
+	widthPercentageToDP as wp,
+	heightPercentageToDP as hp,
+} from "react-native-responsive-screen"
+import { StatusBar } from "expo-status-bar"
+import { LinearGradient } from "expo-linear-gradient"
+
+export default function Index() {
+	return (
+		<View className="flex-1 flex justify-end">
+			<StatusBar style="dark" />
+			<Image
+				className="h-full w-full absolute"
+				source={require("../assets/images/gym1.jpg")}
+			/>
+
+			<LinearGradient
+				colors={["transparent", "#18181b"]}
+				style={{ width: wp(100), height: hp(70) }}
+				start={{ x: 0.5, y: 0 }}
+				end={{ x: 0.5, y: 0.8 }}
+				className="flex justify-end pb-12 space-y-8"
+			>
+				<View className="flex items-center">
+					<Text
+						style={{ fontSize: hp(5) }}
+						className="text-white font-bold tracking-wide"
+					>
+						Best <Text className="text-rose-500">Workouts</Text>
+					</Text>
+					<Text
+						style={{ fontSize: hp(4) }}
+						className="text-white font-bold tracking-wide"
+					>
+						For you
+					</Text>
+				</View>
+
+				<View>
+					<TouchableOpacity
+						style={{ height: hp(7), width: wp(80) }}
+						className="bg-rose-500 flex items-center justify-center mx-auto rounded-full border-[1px] border-neutral-200"
+					>
+						<Text
+							style={{ fontSize: hp(3) }}
+							className="text-white font-bold tracking-widest"
+						>
+							Get Started
+						</Text>
+					</TouchableOpacity>
+				</View>
+			</LinearGradient>
+		</View>
+	)
+}
