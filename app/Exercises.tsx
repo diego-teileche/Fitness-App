c06325dc108cbb55677dea9526ebef72eb1bf1e6
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-05-05 20:19:00 -0300

    Exercises | Router back added

--- app/Exercises.tsx
+++ app/Exercises.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native"
+import { View, Text, TouchableOpacity } from "react-native"
 import React from "react"
+import { useRouter } from "expo-router"
 
 export default function Exercises() {
+	const router = useRouter()
+
 	return (
-		<View>
+		<View className="mt-20">
 			<Text>Exercises</Text>
+
+			<TouchableOpacity onPress={() => router.back()}>
+				<Text>Go back</Text>
+			</TouchableOpacity>
 		</View>
 	)
 }
